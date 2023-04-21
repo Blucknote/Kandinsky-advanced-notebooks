21d7d9492c7b211ee6046e9e7386a68a3522c2f2
root <root@kekw.localdomain>
2023-04-21 21:25:26 +0300

    unlimited images mix

--- ui/imgsmix_ui.py
+++ ui/imgsmix_ui.py
@@ -4,67 +4,89 @@ from PIL import Image
 import ui
 import main
 
-img1 = ui.widgets.FileUpload(
-    accept='image/*',
-    multiple=False,
-)
-img2 = ui.widgets.FileUpload(
-    accept='image/*',
-    multiple=False,
-)
+
+style = {'description_width': 'initial', 'padding': '10px'}
+mix = ui.widgets.Button(description="Mix")
+
+constructor = ui.widgets.Output()
+
+class ImageUploadConstructor:
+    upload_buttons = []
+    weights = []
+    boxes = {}
+    
+    @constructor.capture()
+    def create_button(self, event):
+        btn = ui.widgets.FileUpload(
+            accept='image/*',
+            multiple=False,
+        )
+        self.upload_buttons.append(btn)
+        weight = ui.widgets.FloatSlider(
+            value=0.5,
+            min=0.0,
+            max=2.0,
+            step=0.1,
+            description='IMG weigth:',
+            readout_format='.1f',
+        )
+        self.weights.append(weight)
+
+        btn.observe(self.generate_preview, names='value')
+        
+        deleter = ui.widgets.Button(description="Delete image from mix", style=style)
+        deleter.on_click(self.delete)
+        
+        box = ui.widgets.widgets.VBox([ui.widgets.Output(), btn, weight, deleter])
+        display(box)
+        self.boxes[id(btn)] = box
+        mix.description = f"Mix ({len(self.upload_buttons)}) images"
+        
+    def generate_preview(self, change):
+        out = self.boxes[id(change["owner"])].children[0]
+        out.clear_output(wait=True)
+        pre = ui.widgets.Image(
+            value=change["new"][0]["content"],
+            format='png',
+            width=350,
+            height=350,
+        )
+        with out:
+            display(pre)
+    
+    def get_pills(self):
+        pills = []
+        for notpill in self.upload_buttons:
+            _bytes = BytesIO(notpill.value[0]["content"].tobytes())
+            pills.append(Image.open(_bytes))
+        return pills
+    
+    def get_weights(self):
+        return [w.value for w in self.weights]
+    
+    def delete(self, event):
+        for btn, box in self.boxes.items():
+            if box.children[-1] is event:
+                box.close()
+                break
+                
+        for index, upload_btn in enumerate(self.upload_buttons):
+            if id(upload_btn) == btn:
+                break
+                
+        self.upload_buttons.pop(index)
+        self.weights.pop(index)
+        mix.description = f"Mix ({len(self.upload_buttons)}) images"
 
 
-preview = ui.widgets.Output()
-
-@preview.capture()
-def gen_preview(change):
-    pre = ui.widgets.Image(
-        value=change["new"][0]["content"],
-        format='png',
-        width=350,
-        height=350,
-    )
-    display(pre)
-
-img1.observe(gen_preview, names='value')
-img2.observe(gen_preview, names='value')
-
-display(preview)
-
-# display(ui.widgets.Box([preview1, preview2]))
-
-def get_pills():
-    raw1 = BytesIO(img1.value[0]["content"].tobytes())
-    pil1 = Image.open(raw1)
-    raw2 = BytesIO(img2.value[0]["content"].tobytes())
-    pil2 = Image.open(raw2)
-    return [pil1, pil2]
-
-img_w1 = ui.widgets.FloatSlider(
-    value=0.5,
-    min=0.0,
-    max=2.0,
-    step=0.1,
-    description='IMG1 weigth:',
-    readout_format='.1f',
-)
-
-
-img_w2 = ui.widgets.FloatSlider(
-    value=0.5,
-    min=0.0,
-    max=2.0,
-    step=0.1,
-    description='IMG2 weigth:',
-    readout_format='.1f',
-)
-
-img_box1 = ui.widgets.widgets.VBox([img1, img_w1])
-img_box2 = ui.widgets.widgets.VBox([img2, img_w2])
-
-imgs_box = ui.widgets.HBox([img_box1, img_box2])
-display(imgs_box)
-
+display(constructor)
+uploader_constructor = ImageUploadConstructor()
+add_img = ui.widgets.Button(description="Add image to mix")
+uploader_constructor.create_button(None)
+uploader_constructor.create_button(None)
+add_img.on_click(uploader_constructor.create_button)
+display(add_img)
+    
 def center_crop(image):
     width, height = image.size
     new_size = min(width, height)
@@ -85,8 +107,8 @@ def imgmix(event):
     images = []
     for cur_iter in range(ui.n_iter.value):
         image_iter = main.model.mix_images(
-            get_pills(),
-            [img_w1.value, img_w2.value], 
+            uploader_constructor.get_pills(),
+            uploader_constructor.get_weights(), 
             num_steps=ui.steps.value,
             batch_size=ui.batch_size.value,
             guidance_scale=ui.cfg_scale.value,
@@ -109,7 +131,6 @@ def imgmix(event):
 ui.prompts.close()
 ui.generate.close()
 
-mix = ui.widgets.Button(description="Mix")
 mix.on_click(imgmix)
 display(mix)
 display(out)
